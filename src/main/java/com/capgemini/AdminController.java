5badff44f10a44a23ee112db6aa240264d5545c2
Himayan-Singha <himayan.singha@gmail.com>
2021-05-19 18:26:13 +0530

    MetaData Cleanup API added

--- src/main/java/com/capgemini/AdminController.java
+++ src/main/java/com/capgemini/AdminController.java
@@ -1,6 +1,7 @@
 package com.capgemini;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,27 +9,54 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.entities.ServiceRequest;
 import com.capgemini.entities.ServiceRequestDetail;
-//import com.capgemini.repository.ServiceCatalogRepository;
+import com.capgemini.repository.EnquiryRepository;
+import com.capgemini.repository.MechanicsRepository;
+import com.capgemini.repository.ServiceCatalogRepository;
 import com.capgemini.repository.ServiceRequestDetailRepository;
 import com.capgemini.repository.ServiceRequestRepository;
+import com.capgemini.repository.ServiceTypeRepository;
+import com.capgemini.repository.UserDetailsRepository;
+import com.capgemini.repository.VehicleRepository;
+import com.capgemini.repository.VehicleTypeRepository;
 
 @RestController
 @RequestMapping("/api/admin/")
 public class AdminController {
 
 	@Autowired
+	private EnquiryRepository enquiryrepository;
+	
+	@Autowired
+	private MechanicsRepository mechanicrepository;
+	
+	@Autowired
+	private ServiceCatalogRepository sercatrepository;
+
+	@Autowired
+	private ServiceRequestRepository serreqrepository;
+	
+	@Autowired
 	private ServiceRequestDetailRepository srdrepository;
 	
 	@Autowired
-	private ServiceRequestRepository ser_reqrepository;
+	private ServiceTypeRepository servicetyperepository;
 	
-	/*@Autowired
-	private ServiceCatalogRepository sercatrepository;*/
+	@Autowired
+	private UserDetailsRepository userdetailrepository;
+	
+	@Autowired
+	private VehicleRepository vehiclerepository;
+	
+	@Autowired
+	private VehicleTypeRepository vehicletyperepository;
+	
+	
+	
 	
 	@PostMapping("/generatedetail")
 	public String generateRequestdetail(@RequestBody ServiceRequestDetail servicerequestdetail) {
 		if(servicerequestdetail.getService_request()!=null) {
-			ServiceRequest serreq=ser_reqrepository.findById(servicerequestdetail.getService_request().getService_req_id()).get();
+			ServiceRequest serreq=serreqrepository.findById(servicerequestdetail.getService_request().getService_req_id()).get();
 			servicerequestdetail.setService_request(serreq);
 		}
 		/*if(servicerequestdetail.getServicecatalog()!=null) {
@@ -39,5 +67,22 @@ public class AdminController {
 		srdrepository.save(servicerequestdetail);
 		return "Service Details generated!!!";
 	}
+	
+	
+	@DeleteMapping("/metaclean")
+	public String metaDataCleanUp() {
+		enquiryrepository.deleteEnquiryByIsDelete(true);
+		serreqrepository.deleteServiceRequestByIsDelete(true);
+		vehiclerepository.deleteVehicleByIsDelete(true);
+		vehicletyperepository.deleteVehicleTypeByIsDelete(true);
+		sercatrepository.deleteServiceCatalogByIsDelete(false);
+		servicetyperepository.deleteServiceTypeByIsDelete(true);
+		mechanicrepository.deleteMechanicByIsDelete(true);
+		userdetailrepository.deleteUserDetailsByIsDelete(true);
+		
+		return "Done";
+	}
+	
+	
 	
 }
--- src/main/java/com/capgemini/repository/ServiceRequestDetailRepository.java
+++ src/main/java/com/capgemini/repository/ServiceRequestDetailRepository.java
@@ -1,9 +1,18 @@
 package com.capgemini.repository;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.capgemini.entities.ServiceRequestDetail;
 
 public interface ServiceRequestDetailRepository extends JpaRepository<ServiceRequestDetail,Integer> {
+	
+	@Transactional
+	@Modifying
+	@Query(value = "DELETE from ServiceRequestDetail where is_deleted = true")
+	void deleteServiceRequestDetailByIsDelete(boolean isDeleted);
 
 }
